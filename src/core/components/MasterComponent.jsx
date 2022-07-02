b1f30dc6e78acbebccd16664e412eecda7790caf
iampratiktandel <iampratiktandel@gmail.com>
2022-07-02 23:11:46 +0530

    feat: app setup

---
+++ src/core/components/MasterComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginComponent from './LoginComponent';
+
+
+class MasterComponent extends React.Component {
+  render() {
+    return <LoginComponent></LoginComponent>;
+  }
+}
+
+export default MasterComponent;
